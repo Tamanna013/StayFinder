dfddb93958274bc765e73a667611e6020a6f6f41
TashxMG <149794391+Tamanna013@users.noreply.github.com>
2025-06-18 20:44:47 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import { AuthProvider } from './context/authContext'; // Import AuthProvider
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider> {/* Wrap App with AuthProvider */}
+    <AuthProvider> 
       <App />
     </AuthProvider>
   </React.StrictMode>
